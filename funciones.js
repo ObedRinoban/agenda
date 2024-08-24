4e9a2c565b6ec5d9fa846b7e67c360a8e4157979
sistemaasistencia <obed.sh@gmail.com>
2024-08-24 18:41:15 -0400

    Sistema de Agenda

--- funciones.js
+++ funciones.js
@@ -7,3 +7,10 @@ function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos()
+{
+    let hoja = SpreadsheetApp.openById('1ed6NANH7_9Ie0o_KbAOcDsf0B2Zhcc-k1PGuBubUrpM').getActiveSheet();
+    let  datos = hoja.getDataRange().getValues();
+    return datos;
+}
